1957a68dc843224af6a69509d2e1eee3dca212da
UncleSamsun <alswns5620@naver.com>
2022-01-05 20:12:22 +0900

    2일차: Module and Package Project, Module, Package 개념 이해 및 사용 방법 학습

--- src/main/kotlin/HelloKotlin.kt
+++ src/main/kotlin/HelloKotlin.kt
@@ -1,3 +1,14 @@
+import com.example.edu.Person as User
+
 fun main() {
     println("Hello Kotlin!")
+
+    val person = User( name = "Kildong", age =  20)
+    val person2 = Person(no = 123, name = "Kildong", jobTitle = "Programmer")
+
+    println(person)
+    println(person.name)
+    println(person.age)
 }
+
+class Person(val no: Int, val name: String, val jobTitle: String)
